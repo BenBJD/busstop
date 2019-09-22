2f335468c8d76e6dcb580021a3722e9b86bdf697
Unknown <ben@bjdavison.co.uk>
2019-09-23 00:18:49 +0100

    More work

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import libbusstop
 app = Flask(__name__)
 
 # Homepage
 @app.route("/")
 def homepage():
-    return render_template('index.html')
-
-# Buses at a stop
-@app.route("/stop/<ATCO>/<type>")
-# def stop_next_bus(ATCO, type):
-#     if type == "live":
-#         date_time = "live.json"
-#     else:
-#         date_time = type.replace("+", "/") + "/timetable.json"
-#     url = urllib.request.urlopen("http://transportapi.com/v3/uk/bus/stop/" + ATCO + "/" + date_time + "?app_id=b13c7b6d&app_key=077475e1973c1ff5bb3d52e0ba6e63ca")
-#     data = json.loads(url.read())
-#     return data
-def nextBus(ATCO, type):
-    # Find next bus
+    return render_template("index.html")
 
 
+@app.route("/search", methods=["POST"])
+def search():
+    search_term = request.form["q"]
+    option = request.form["o"]
+    search_request = libbusstop.NewRequest(search_term, option)
+    results = search_request.search_db()
+    print(search_term, option)
+    print(results)
+    return render_template("search.html", results=results, len_results=len(results), search_term=search_term)
 
-if __name__ == '__main__':
+
+@app.route("/stop/<ATCO>/time/next_bus")
+def next_bus(ATCO):
+    pass
+
+
+@app.route("/stop/<ATCO>/time/<time>")
+def time_bus(ATCO, time):
+    pass
+
+
+if __name__ == "__main__":
     app.run()
--- libbusstop.py
+++ libbusstop.py
@@ -2,35 +2,35 @@ import sqlite3
 import urllib.request
 import json
 
-class GetATCO:
-    # Initialising
-    def __init__(self, searchTerm, searchTermType):
-        self.searchTerm = searchTerm
-        self.searchTermType = searchTermType
-        conn = sqlite3.connect(naptan_data.db)
-        self.cursor = conn.cursor()
 
-    def search(self):
-        # Search database for stops with either NaPTAN code or search term
-        pass
+class NewRequest:
+    def __init__(self, search_term, search_term_type):
+        self.search_term = search_term
+        self.search_term_type = search_term_type
+        self.atco_code = ""
+        self.api_key = "077475e1973c1ff5bb3d52e0ba6e63ca"
+        self.app_id = "b13c7b6d"
+        conn = sqlite3.connect("naptan_data.db")
+        self.c = conn.cursor()
 
-    def process(self):
-        # Format stops into ATCO and location data, then close database connection
-        pass
-
-class GetData:
-    # Initialising
-    def __init__(self, ATCO, searchType):
-        self.searchType = searchType
-        self.ATCO = ATCO
-        self.busService = {
-            "inbound": "",
-            "outbound": "",
-            "fullName": ""
-        }
-
-    api_key = "077475e1973c1ff5bb3d52e0ba6e63ca"
-    app_id = "b13c7b6d"
-
-    def getOperator(self):
-        url = "http://transportapi.com/v3/uk/bus/stop/" + self.ATCO
+    def search_db(self):
+        # Search database for stops with either naptan code or search term
+        results = []
+        if self.search_term_type == "2":
+            self.c.execute(f"SELECT * FROM stops WHERE NaptanCode = '{self.search_term}'")
+            results = self.c.fetchall()
+        elif self.search_term_type == "1":
+            self.c.execute(f"""
+            SELECT *
+            FROM stops 
+            WHERE CommonName LIKE '%{self.search_term}%'
+            OR Landmark LIKE '%{self.search_term}%'
+            OR Street LIKE '%{self.search_term}%'
+            OR LocalityName LIKE '%{self.search_term}%'
+            """)
+        for j in self.c.fetchall():
+            j = list(j)
+            del j[4]
+            results.append(j)
+        # Results saved in "results"
+        return results
